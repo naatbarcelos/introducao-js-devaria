8d9177550095fb16c57ceec88c520b788bd26e2e
naatbarcelos <naathalia.b@hotmail.com>
2024-01-10 19:56:34 -0300

    Aula variáveis, escopo e funções

--- js/segregacao.js
+++ js/segregacao.js
@@ -1,10 +1,19 @@
-console.log('Olá mundo, introdução a JS');
-console.log(`Data hora: ${new Date()}`);
+const OlaMundo = 'Olá Mundo, introdução a JS';
+const DataAtual = new Date();
 
-function mensagem(){
-    alert ('Chamamos um modal')
+console.log('Olá mundo, introdução a JS');
+console.log(`Data hora: ${DataAtual}`);
+
+function mensagem(mensagem){
+    alert ('mensagem');
 }
 
 function mensagem2(){
-    alert ('mensagem 2')
+    mensagem('Aprendendo funções');
 }
+
+const mensagemFinal = (mensagemFinal) => {
+    console.log(mensagemFinal);
+}
+
+mensagemFinal('Concluimos a execução');
