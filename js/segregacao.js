b846e66ec9425d299fb87f9bfed6119720054e77
naatbarcelos <naathalia.b@hotmail.com>
2024-01-10 17:43:08 -0300

    Bootstrap Intro

--- js/segregacao.js
+++ js/segregacao.js
@@ -1,6 +1,10 @@
 console.log('Olá mundo, introdução a JS');
 console.log(`Data hora: ${new Date()}`);
 
-function mensagem (){
+function mensagem(){
     alert ('Chamamos um modal')
+}
+
+function mensagem2(){
+    alert ('mensagem 2')
 }
