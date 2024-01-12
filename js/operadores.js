06d444e809fb3b981e39da89d39dae3e3743b0e3
naatbarcelos <naathalia.b@hotmail.com>
2024-01-12 11:13:42 -0300

    Aula Estrutura de Seleção

--- js/operadores.js
+++ js/operadores.js
@@ -2,7 +2,7 @@
 const numero1 = 32;
 const numero2 = 24;
 const numero3 = 14;
-const numero4 = 1;
+const numero4 = 89;
 const stringNumero = '1';
 
 console.log('é igual', numero4 == stringNumero);
@@ -17,4 +17,25 @@ console.log('Adição:', numero1 + numero2);
 console.log('Subtração:', numero4 - numero1);
 console.log('Multiplicação:', numero3 * numero1);
 console.log('Divisão:', numero1 / numero3);
-console.log('Módulo:', numero2 % numero3)
+console.log('Módulo:', numero2 % numero3);
+
+if(numero3 > numero4){
+    console.log('Numero 3 é maior');
+}else if(numero2 > numero3){
+    console.log('Número 2 é maior');
+}else{
+        console.log('Número 4 é maior');
+    }
+
+switch(numero4){
+    case 65:
+        console.log('sessenta e cinco');
+        break
+    case 89:
+        console.log('oitenta e nove');
+        break
+    case 32:
+        console.log('trinta e dois');
+    default:
+        console.log('Número não encontrado');
+}
