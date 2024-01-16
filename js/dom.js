8f529702d0bda81fe62ae440c7e0f23c5b298de2
naatbarcelos <naathalia.b@hotmail.com>
2024-01-16 19:24:37 -0300

    Aula Manipulando DOM Parte 2

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,33 @@
 const divs= document.getElementsByTagName('div');
-console.log(divs);
+//console.log(divs);
 const h2s = document.getElementsByTagName('h2');
-console.log(h2s);
+//console.log(h2s);
 const titulo = document.getElementById('titulo');
-console.log(titulo);
+//console.log(titulo);
+const input = document.getElementById('input');
+const paragrafo = document.getElementById('paragrafo');
 const elementsMarginTop2 = document.getElementsByClassName('mt-2');
-console.log(elementsMarginTop2);
+//console.log(elementsMarginTop2);
 
 function alterarHtml(){
-    titulo.textContent = 'Novo título';
+    titulo.textContent = input.value;
     titulo.style.color = '#ac4389';
-    //elementsMarginTop2.className = 'mt-4';
+    for(let element of elementsMarginTop2){
+        element.className = 'mt-4';
+    }
+    input.value = '';
+
+    const novoP = document.createElement('p');
+    novoP.textContent = 'Novo texto adicionado via JavaScrip DOM';
+
+    paragrafo.parentNode.appendChild(novoP);
 }
 
+// function alterarHtml(){
+//     titulo.textContent = 'Novo título';
+//     titulo.style.color = '#ac4389';
+//     for(let element of elementsMarginTop2){
+//         element.className = 'mt-4';
+//     }
+//     //elementsMarginTop2.className = 'mt-4';
+// 
